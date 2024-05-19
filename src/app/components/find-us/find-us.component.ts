59e332cd280e341041b8e34759d9ff151e33702a
wachtelhund <hampus.nilsson222@hotmail.se>
2024-05-19 13:17:58 +0200

    Add map

--- src/app/components/find-us/find-us.component.ts
+++ src/app/components/find-us/find-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as L from "leaflet";
+import 'leaflet/dist/leaflet.css';
 
 @Component({
   selector: 'app-find-us',
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './find-us.component.scss'
 })
 export class FindUsComponent {
+  map!: L.Map;
+  location: L.LatLngExpression = [56.59337504675528, 16.16079803139656];
+  ngOnInit() {
+    this.initMap();
+  }
 
+  initMap() {
+    this.map = L.map('map').setView(this.location, 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    L.marker(this.location).addTo(this.map)
+      .bindPopup('More Paintball')
+      .openPopup();
+  }
 }
