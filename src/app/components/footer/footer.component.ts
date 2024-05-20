6275447ff17206858cf12968cd5effbd378c6cb5
Hampus Tuisku <ht222mi@student.lnu.se>
2024-05-20 13:22:30 +0200

    Add solution to display svg logos

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,16 +1,34 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatButtonModule,
+    RouterModule,
+    MatIconModule,
+    HttpClientModule
+  ],
+  providers: [MatIconRegistry],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  instagramLink = 'https://www.instagram.com/more_paintball/';
+  facebookLink = 'https://www.facebook.com/people/More-Paintball/61553086056125/'
 
-  instagramLink(): void {
-    const instagramLink = 'https://www.instagram.com/more_paintball/';
-    window.open(instagramLink, '_blank');
+  constructor(private iconService: MatIconRegistry, private domSanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this.iconService.addSvgIcon('instagram', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/instagram.svg'));
+    this.iconService.addSvgIcon('facebook', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/facebook.svg'));
   }
 }
