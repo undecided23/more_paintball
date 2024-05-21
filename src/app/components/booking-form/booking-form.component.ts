a9fb5eff0aaed497ae8c2290e9b953d27d1441d2
Hampus Tuisku <ht222mi@student.lnu.se>
2024-05-21 15:35:21 +0200

    Add functionality to send booking

--- src/app/components/booking-form/booking-form.component.ts
+++ src/app/components/booking-form/booking-form.component.ts
@@ -9,6 +9,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { Booking } from '../../../types/booking.model'
 import { BookerType } from '../../../types/booking.model'
+import { MailingService } from '../../services/mailing.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-booking-form',
@@ -21,7 +23,8 @@ import { BookerType } from '../../../types/booking.model'
     ReactiveFormsModule,
     MatDatepickerModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './booking-form.component.html',
@@ -30,19 +33,49 @@ import { BookerType } from '../../../types/booking.model'
 export class BookingFormComponent {
   errorMessage = '';
 
+  bookerTypes = [
+    { value: BookerType.PRIVATE, viewValue: 'Privatperson' },
+    { value: BookerType.COMPANY, viewValue: 'Företag' }
+  ];
+
   bookingForm = new FormGroup({
-    booker_name : new FormControl('', Validators.required),
-    booker_email: new FormControl('', Validators.required),
-    booker_phone: new FormControl('', Validators.required),
-    booking_date: new FormControl('', Validators.required),
-    number_of_people: new FormControl('', Validators.required),
-    booker_message: new FormControl('', Validators.required),
-    booker_type: new FormControl('', Validators.required)
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl(0, [Validators.required]),
+    date: new FormControl(new Date(), Validators.required),
+    people: new FormControl(0, Validators.required),
+    message: new FormControl('', Validators.required),
+    booker_type: new FormControl(BookerType.PRIVATE, Validators.required)
   });
 
-  constructor () {}
+  constructor (private mailingService: MailingService) {}
 
   sendBooking(booking: Booking) {
-    
+    this.bookingForm.value
+    this.mailingService.sendBookingRequest(booking)
+      .then(() => {
+        console.log('Booking request sent');
+        
+      })
+      .catch((error) => {
+        this.errorMessage = error;
+      });
+  }
+
+  handleBooking() {
+    if (this.bookingForm.valid) {
+      const data: Booking = {
+        booker_name: this.bookingForm.value.name!,
+        booker_email: this.bookingForm.value.email!,
+        booker_phone: this.bookingForm.value.phone!,
+        booking_date: this.bookingForm.value.date!,
+        number_of_people: this.bookingForm.value.people!,
+        booker_message: this.bookingForm.value.message!,
+        booker_type: this.bookingForm.value.booker_type!
+      }
+      this.sendBooking(data)
+    } else {
+      this.errorMessage = 'Formuläret är inte korrekt ifyllt';
+    }
   }
 }
