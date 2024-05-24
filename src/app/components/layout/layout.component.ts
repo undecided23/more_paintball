bb444f12638826dd4f431c38c4d13e6531567ca9
JonathanTobiasFlores <jonathan.tobias.flores@gmail.com>
2024-05-24 17:11:41 +0200

    Add about us component

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FindUsComponent } from '../find-us/find-us.component';
 import { HomeComponent } from '../home/home.component';
+import { AboutUsComponent } from '../about-us/about-us.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     FindUsComponent,
-    HomeComponent
+    HomeComponent,
+    AboutUsComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
